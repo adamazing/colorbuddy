ded638424098924ce83dd2516d661785bd9ec9e7
Adam Henley <adamazing@gmail.com>
2022-10-18 02:29:42 +1300

    Add K-Means quantisation method
    Signed-off-by: Adam Henley <adamazing@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::fmt;
 use std::path::*;
 
+use mcq::ColorNode;
 use mcq::MMCQ;
+use exoquant::Color as ExoquantColor;
+use exoquant::{generate_palette, Histogram, optimizer, SimpleColorSpace};
 use anyhow::Result;
 use clap::{Parser, ValueEnum};
 
@@ -11,11 +14,11 @@ const COLOR_HEIGHT: u32 = 256;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None,trailing_var_arg=true)]
 struct Args {
-    #[arg(short='m', long="quantisation-method", default_value_t=QuantisationMethod::MedianCut)]
+    #[arg(short='m', long="quantisation-method", default_value_t=QuantisationMethod::KMeans)]
     quantisation_method: QuantisationMethod,
 
     #[arg(short='n', long="number-of-colours", default_value="8")]
-    number_of_colours: u32,
+    number_of_colours: usize,
 
     #[arg(long, short='i')]
     image: String
@@ -36,6 +39,12 @@ impl fmt::Display for QuantisationMethod {
     }
 }
 
+struct Color {
+    pub r: u8,
+    pub g: u8,
+    pub b: u8
+}
+
 fn main() -> Result<()>  {
     let matches = Args::parse();
 
@@ -43,31 +52,72 @@ fn main() -> Result<()>  {
     println!("{:?}", matches.number_of_colours);
 
     let image = matches.image;
-    process_image(&image, &matches.number_of_colours);
+    process_image(&image, &matches.number_of_colours, &matches.quantisation_method);
 
     Ok(())
 }
 
+fn mcq_color_nodes_to_local_colors(mcq_color_nodes: Vec<ColorNode>) -> Vec<Color> {
+    let mut local_colors: Vec<Color> = vec![];
+
+    for c in mcq_color_nodes {
+        local_colors.push(Color{
+            r: c.red,
+            g: c.grn,
+            b: c.blu
+        })
+    }
+
+    return local_colors;
+}
+
+fn exoquant_colors_to_local_colors(exoquant_colors: Vec<ExoquantColor>) -> Vec<Color> {
+    let mut local_colors: Vec<Color> = vec![];
+
+    for c in exoquant_colors {
+        local_colors.push(Color{
+            r: c.r,
+            g: c.g,
+            b: c.b
+        })
+    }
+
+    return local_colors;
+}
 
 /**
- * Process Image function
  * Writes an output image consisting of the original image, with a palette of colours shown
  * along the bottom.
  *
- * [String] filename of the image to process
- * [u32] Size of the palette to generate
+ * [String] filename of the image to process.
+ * [u32] Size of the palette to generate.
+ * [QuantisationMethod] The quantisation method to use.
  */
-fn process_image(file: &String, number_of_colours: &u32) {
+fn process_image(file: &String, number_of_colours: &usize, quantisation_method: &QuantisationMethod) {
     println!("Reading image {}", file);
 
     let dynamic_image = image::open(file).unwrap();
     let input_image = dynamic_image.to_rgba8();
-    let data = input_image.clone().into_vec();
-    let mcq = MMCQ::from_pixels_u8_rgba(data.as_slice(), *number_of_colours);
+    let qc: Vec<Color>;
 
-    // A `Vec` of colors, sorted by usage frequency descending
-    let qc = mcq.get_quantized_colors();
+    match quantisation_method {
+        QuantisationMethod::MedianCut => {
+            let data = input_image.clone().into_vec();
+            let mcq = MMCQ::from_pixels_u8_rgba(data.as_slice(), (*number_of_colours).try_into().unwrap());
 
+            qc = mcq_color_nodes_to_local_colors(mcq.get_quantized_colors().to_vec());
+        },
+        QuantisationMethod::KMeans => {
+            let histogram: Histogram = input_image.clone().pixels().map(|p| ExoquantColor {r: p[0], g: p[1], b: p[2], a: p[3] }).collect();
+            qc = exoquant_colors_to_local_colors(generate_palette(
+                    &histogram,
+                    &SimpleColorSpace::default(),
+                    &optimizer::KMeans,
+                    *number_of_colours
+                )
+            )
+        }
+    }
     let (input_image_width, input_image_height) = input_image.dimensions();
 
     // Create an image buffer big enough to hold the output image
@@ -81,15 +131,15 @@ fn process_image(file: &String, number_of_colours: &u32) {
     }
 
     // The width of each color in the palette strip
-    let color_width = input_image_width / number_of_colours;
+    let color_width = input_image_width / *number_of_colours as u32;
 
     for y in (input_image_height + 1)..(input_image_height + COLOR_HEIGHT) {
         for x0 in 0..*number_of_colours {
-            let x1 = x0 * color_width;
-            let q = qc[x0 as usize];
+            let x1 = x0 as u32 * color_width;
+            let q = &qc[x0 as usize];
 
             for x2 in 0..color_width {
-                imgbuf.put_pixel(x1 + x2, y, image::Rgba([q.red, q.grn, q.blu, 0xff]));
+                imgbuf.put_pixel(x1 + x2, y, image::Rgba([q.r, q.g, q.b, 0xff]));
             }
         }
     }
